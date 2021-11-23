9a454220dc8807e3adad20f02685c7ca9278409f
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-23 23:24:07 +0300

    PostsService was created.

--- app/src/main/java/com/metehanbolat/ktorclientandroid/data/remote/PostServiceImplementation.kt
+++ app/src/main/java/com/metehanbolat/ktorclientandroid/data/remote/PostServiceImplementation.kt
@@ -5,6 +5,7 @@ import com.metehanbolat.ktorclientandroid.data.remote.dto.PostResponse
 import io.ktor.client.*
 import io.ktor.client.features.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import java.lang.Exception
 
 class PostServiceImplementation(private val client: HttpClient) : PostsService {
@@ -30,6 +31,27 @@ class PostServiceImplementation(private val client: HttpClient) : PostsService {
     }
 
     override suspend fun createPost(postRequest: PostRequest): PostResponse? {
-        TODO("Not yet implemented")
+        return try {
+            client.post<PostResponse>{
+                url(HttpRoutes.POSTS)
+                contentType(ContentType.Application.Json)
+                body = postRequest
+            }
+        } catch (e : RedirectResponseException){
+            // 3xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e : ClientRequestException){
+            // 4xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e : ServerResponseException){
+            // 5xx - responses
+            println("Error: ${e.response.status.description}")
+            null
+        } catch (e :  Exception){
+            println("Error: ${e.message}")
+            null
+        }
     }
 }
--- app/src/main/java/com/metehanbolat/ktorclientandroid/data/remote/PostsService.kt
+++ app/src/main/java/com/metehanbolat/ktorclientandroid/data/remote/PostsService.kt
@@ -2,9 +2,29 @@ package com.metehanbolat.ktorclientandroid.data.remote
 
 import com.metehanbolat.ktorclientandroid.data.remote.dto.PostRequest
 import com.metehanbolat.ktorclientandroid.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 
 interface PostsService {
 
     suspend fun getPosts() : List<PostResponse>
     suspend fun createPost(postRequest: PostRequest) : PostResponse?
+
+    companion object {
+        fun create() : PostsService {
+            return PostServiceImplementation(
+                client = HttpClient(Android){
+                    install(Logging){
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature){
+                        serializer = KotlinxSerializer()
+                    }
+                }
+            )
+        }
+    }
 }
